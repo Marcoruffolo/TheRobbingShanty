70a64e941b66b97dcde065f6b4589337303f4217
conriancon <conriancon@hotmail.com>
2026-05-24 14:53:35 -0300

    Energia durabilidad

--- Assets/Scripts/Ship/Durability/ShipDurability.cs
+++ Assets/Scripts/Ship/Durability/ShipDurability.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class ShipDurability : MonoBehaviour
 {
     [SerializeField] private SOVariableFloat durability;
+    [SerializeField] private Material energyMaterial;
     [SerializeField] private float drainRatePerSecond = 1f;
 
     private void Update()
     {
+        energyMaterial.SetFloat("_Durability", durability.Value);
         if (CameraModeController.Instance != null && CameraModeController.Instance.IsShipControlActive)
             durability.Add(-drainRatePerSecond * Time.deltaTime);
+
     }
 }
