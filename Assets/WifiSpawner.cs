cd17b44bacf6e5f081382f6acb3c26fcf9a525a1
conriancon <conriancon@hotmail.com>
2026-06-23 12:32:27 -0300

    Fixes

--- Assets/WifiSpawner.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using System.Collections.Generic;
-
-public class WifiSpawner : MonoBehaviour
-{
-    public List<Transform> _wifiSpawnpoints;
-    public GameObject _wifiStick;
-}
--- Assets/WifiStick.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class WifiStick : MonoBehaviour
-{
-    public bool TurnedOn = false;
-
-    public Material _lightMat;
-    
-}
