4e795cce0324146969a0f3ef0107735fc18bbf65
conriancon <conriancon@hotmail.com>
2026-07-22 14:15:50 -0300

    UpgradeMenu Remake start

---
+++ Assets/ChalkBoardMenuManager.cs
@@ -0,0 +1,35 @@
+using Unity.Cinemachine;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChalkBoardMenuManager : MonoBehaviour, IGameEventListener<bool>
+
+{
+    public BoolGameEvent OnUpgradeUIOpen;
+
+    public GameObject menucamera;
+
+    public GameObject mainMenu;
+    public GameObject mapMenu;
+
+    public GraphicRaycaster interactablemenu;
+
+    private void OnEnable()
+    {
+        OnUpgradeUIOpen.RegisterListener(this);
+    }
+
+    private void OnDisable()
+    {
+        OnUpgradeUIOpen.UnregisterListener(this);
+    }
+
+    public void OnEventRaised(bool value)
+    {
+        interactablemenu.enabled = value;
+        menucamera.GetComponent<CinemachineCamera>().enabled = value;
+        Debug.Log($"Upgrade UI open: {value}");
+    }
+
+}
---
+++ Assets/ChalkBoardStation.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ChalkBoardStation : MonoBehaviour, IInteractable
+{
+    [SerializeField] private string interactionPrompt = "Mejorar Items";
+    public string InteractionPrompt => interactionPrompt;
+    public UnityAction<IInteractable> OnInteractionComplete { get; set; }
+
+    public BoolGameEvent OnUpgradeUIOpen;
+
+    private void Start() 
+    {
+        OnUpgradeUIOpen?.Raise(false);
+    }
+
+    public void Interact(Interactor interactor, out bool interactSuccessful)
+    {
+        PlayerCamera.LockCursor(false);
+        BlockPlayerMovement.Instance?.ImmobilizePlayer();
+        OnUpgradeUIOpen?.Raise(true);
+        interactSuccessful = true;
+    }
+
+    public void EndInteraction()
+    {
+        PlayerCamera.LockCursor(true);
+        BlockPlayerMovement.Instance?.FreePlayer(); 
+        OnUpgradeUIOpen?.Raise(false);
+    }
+
+    public void Interact() => Interact(null, out _);
+}
